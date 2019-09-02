987aff626c1f86900f9cdc566fd845d2ef73e52d
Darshan P <darshanp@juniper.net>
2019-09-02 17:09:13 +0530

    infix_to_prefix, infix_to_postfix

--- darshan/stacks/infix_to_postfix.py
+++ darshan/stacks/infix_to_postfix.py
@@ -1,2 +1,38 @@
 from pythonds.basic import Stack
+import unittest
 
+
+def infix_to_postfix(exp):
+    stack = Stack()
+    result = ""
+    priority = {'^': 3, '*': 2, '/': 2, '%': 2, '+': 1, '-': 1, '(': 0}
+
+    for i in exp:
+        if i.isalnum() or i.isalpha():
+            result += i
+        elif i == "(":
+            stack.push(i)
+        elif i == ")":
+            while stack.peek() != "(":
+                result += stack.pop()
+            stack.pop()
+        else:
+            while not stack.isEmpty() and priority[stack.peek()] >= priority[i]:
+                result += stack.pop()
+            stack.push(i)
+
+    while not stack.isEmpty():
+        result += stack.pop()
+
+    return result
+
+
+class TestInfixToPostfix(unittest.TestCase):
+    def test_expressions(self):
+        expressions = {"A+B*C+D": "ABC*+D+", "(A+B)*(C+D)": "AB+CD+*", "A*B+C*D": "AB*CD*+", "A+B+C+D": "AB+C+D+"}
+        results = [infix_to_postfix(expression) for expression in expressions]
+        self.assertListEqual(results, list(expressions.values()))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ darshan/stacks/infix_to_prefix.py
@@ -0,0 +1,23 @@
+from darshan.stacks.infix_to_postfix import infix_to_postfix
+import unittest
+
+
+def infix_to_prefix(exp):
+    exp = list(exp[::-1])
+    for i in range(len(exp)):
+        if exp[i] == "(":
+            exp[i] = ")"
+        elif exp[i] == ")":
+            exp[i] = "("
+    return (infix_to_postfix("".join(exp)))[::-1]
+
+
+class TestInfixToPrefix(unittest.TestCase):
+    def test_expressions(self):
+        expressions = {"(A+B)*(C+D)": "*+AB+CD", "A*B+C*D": "+*AB*CD"}
+        results = [infix_to_prefix(expression) for expression in expressions]
+        self.assertListEqual(results, list(expressions.values()))
+
+
+if __name__ == "__main__":
+    unittest.main()
