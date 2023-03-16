97e4f7f40356e931a57e972ab6df3ff085f47642
darshanpyadav93 <darshan.p@nutanix.com>
2023-03-16 19:46:48 +0530

    Add sliding window

--- arrays/sliding_window/longest_contiguous_substring.py
+++ arrays/sliding_window/longest_contiguous_substring.py
@@ -1,3 +1,32 @@
+"""
+Given a string s, find the length of the longest
+substring
+ without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+"""
+
 import math
 
 
@@ -24,9 +53,24 @@ class Solution:
                 # move left to the element after the re-occurrence of the value
                 left = ele_tracker[val] + 1
             ele_tracker[val] = right
-        longest_substring = max(longest_substring, right+1-left)
+        # For last iteration
+        longest_substring = max(longest_substring, right + 1 - left)
         return longest_substring
 
+
+# Others from leetcode
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         left = 0
+#         seen = {}
+#         output = 0
+#
+#         for right, curr in enumerate(s):
+#             if curr in seen:
+#                 left = max(left, seen[curr] + 1)
+#             output = max(output, right - left + 1)
+#             seen[curr] = right
+#
+#         return output
 
 a = Solution()
 print(a.lengthOfLongestSubstring("leviosa"))
@@ -36,3 +80,43 @@ print(a.lengthOfLongestSubstring("abcabcbb"))
 print(a.lengthOfLongestSubstring("bbbbb"))
 print(a.lengthOfLongestSubstring("tmmzuxt"))
 print(a.lengthOfLongestSubstring("dvdf"))
+
+
+class Solution1:
+    def LongestSubstring(self, s: str) -> str:
+        if not s:
+            return 0
+
+        # track elements that have appeared
+        ele_tracker = {}
+        right = left = 0
+        longest_substring = ""
+
+        # move right always
+        # move left only when the val occurs again in the window, it is set to index of val + 1
+        # window type: dynamic
+        # window size = (right-left)
+        for right, val in enumerate(s):
+            # if value has already appeared
+            # if appeared value is within the current sliding window
+            if val in ele_tracker and (left <= ele_tracker[val] < right):
+                # find the longest substring
+                if right - left > len(longest_substring):
+                    longest_substring = s[left: right + 1]
+                # move left to the element after the re-occurrence of the value
+                left = ele_tracker[val] + 1
+            ele_tracker[val] = right
+        # For last iteration
+        if right + 1 - left > len(longest_substring):
+            longest_substring = s[left: right + 1]
+        return longest_substring
+
+
+a = Solution1()
+print(a.LongestSubstring("leviosa"))
+print(a.LongestSubstring("abracadabra"))
+print(a.LongestSubstring("pwwaakew"))
+print(a.LongestSubstring("abcabcbb"))
+print(a.LongestSubstring("bbbbb"))
+print(a.LongestSubstring("tmmzuxt"))
+print(a.LongestSubstring("dvdf"))
---
+++ arrays/sliding_window/longest_substring_with_k_distinct_chars.py
@@ -0,0 +1,196 @@
+"""
+Find the longest substring of a string containing `k` distinct characters
+Google Translate Icon
+Given a string and a positive number k, find the longest substring of the string containing k distinct characters.
+If k is more than the total number of distinct characters in the string, return the whole string.
+
+The problem differs from the problem of finding the longest subsequence with k distinct characters.
+Unlike subsequences, substrings are required to occupy consecutive positions within the original string.
+
+
+For example, consider string abcbbbbbbbdbdbbdcdabd.
+
+For k = 2, o/p is ‘bbbbbbbdbdbbd’
+"""
+
+import math
+
+
+class Solution:
+    """
+    @param s: A string
+    @param k: An integer
+    @return: An integer
+    """
+
+    def length_of_longest_substring_k_distinct(self, s: str, k: int) -> int:
+        # if k > distinct chars in s, return -1
+        if not s:
+            return 0
+
+        if len(set(s)) < k:
+            return -1
+
+        # track letters that
+        char_tracker = {}
+        longest_substring = -math.inf
+        left = 0
+
+        # move right always
+        # track elements count
+        # window type: dynamic
+        # window size = (right-left)
+        # move left when the distinct elements are greater than k until:
+        #       one element is completely eliminated (by tracking it's occurrences)
+        for right, val in enumerate(s):
+            # increase occurrence count by 1
+            if val in char_tracker:
+                char_tracker[val] += 1
+            else:
+                char_tracker[val] = 1
+
+            # completely eliminate one distinct character before moving onto next
+            while len(char_tracker) > k:
+                left_char = s[left]
+                if char_tracker[left_char] > 1:
+                    char_tracker[left_char] -= 1
+                else:
+                    char_tracker.pop(left_char)
+                left += 1
+            longest_substring = max(longest_substring, right - left)
+
+        return longest_substring
+
+
+a = Solution()
+# print(a.length_of_longest_substring_k_distinct("abcbdbdbbdcdabd", 2))
+# print(a.length_of_longest_substring_k_distinct("abcbdbdbbdcdabd", 3))
+# print(a.length_of_longest_substring_k_distinct("abcbdbdbbdcdabd", 4))
+# print(a.length_of_longest_substring_k_distinct("eceba", 2))
+# print(a.length_of_longest_substring_k_distinct("eceba", 3))
+# print(a.length_of_longest_substring_k_distinct("eceba", 4))
+# print(a.length_of_longest_substring_k_distinct("WORLD", 2))
+# print(a.length_of_longest_substring_k_distinct("WORLD", 3))
+# print(a.length_of_longest_substring_k_distinct("WORLD", 4))
+print(a.length_of_longest_substring_k_distinct("gbwkfnqduxwfn", 15))
+
+
+class Solution1:
+    """
+    @param s: A string
+    @param k: An integer
+    @return: An integer
+    """
+
+    def longest_substring_k_distinct(self, s: str, k: int) -> int:
+        # if k > distinct chars in s, return -1
+        if not s:
+            return 0
+
+        if len(set(s)) < k:
+            return -1
+
+        # track letters that
+        char_tracker = {}
+        longest_substring = ""
+        left = 0
+
+        for right, val in enumerate(s):
+            # increase occurrence count by 1
+            if val in char_tracker:
+                char_tracker[val] += 1
+            else:
+                char_tracker[val] = 1
+
+            # completely eliminate one distinct character before moving onto next
+            while len(char_tracker) > k:
+                left_char = s[left]
+                if char_tracker[left_char] > 1:
+                    char_tracker[left_char] -= 1
+                else:
+                    char_tracker.pop(left_char)
+                left += 1
+            if (right - left) > len(longest_substring):
+                longest_substring = s[left:right+1]
+
+        return longest_substring
+
+
+        # Initial solution
+        #
+        # char_tracker = {}
+        # longest_substring = ""
+        # left = right = 0
+        #
+        # for right, val in enumerate(s):
+        #     char_tracker[val] = right
+        #     if len(char_tracker) > k:
+        #         if (right - left) > len(longest_substring):
+        #             longest_substring = s[left:right]
+        #         min_char = min(char_tracker.items(), key=lambda l: l[1])
+        #         left = min_char[1] + 1
+        #         char_tracker.pop(min_char[0])
+        # if (right + 1 - left) > len(longest_substring):
+        #     longest_substring = s[left:right]
+        #
+        # return longest_substring
+
+
+a = Solution1()
+print(a.longest_substring_k_distinct("abcbdbdbbdcdabd", 2))
+print(a.longest_substring_k_distinct("abcbdbdbbdcdabd", 3))
+print(a.longest_substring_k_distinct("abcbdbdbbdcdabd", 4))
+print(a.longest_substring_k_distinct("eceba", 2))
+print(a.longest_substring_k_distinct("eceba", 3))
+print(a.longest_substring_k_distinct("eceba", 4))
+print(a.longest_substring_k_distinct("WORLD", 2))
+print(a.longest_substring_k_distinct("WORLD", 3))
+print(a.longest_substring_k_distinct("WORLD", 4))
+
+# class Solution:
+#
+#     def longestKSubstr(self, String, K):
+#         '''
+#             This method finds the longest substring with at most K
+#             Distinct characters.
+#             input: string and a number k
+#             output: longest substring that has at most k distinct characters
+#         '''
+#         if len(set(s)) < k:
+#             return -1
+#         # define a dictionary that will work as hash table to hold our
+#         # characters and their frequencies.
+#         table = dict()
+#
+#         # two variables will point to the start and end of the window
+#         start, end = 0, 0
+#
+#         # variable to hold the size of the longest substring
+#         longest = 0
+#
+#         # expand the window
+#         for end in range(len(String)):
+#             # get the new character
+#             newCharacter = String[end]
+#
+#             # add the new character in the hash table
+#             if newCharacter in table.keys():
+#                 table[newCharacter] += 1
+#             else:
+#                 table[newCharacter] = 1
+#
+#             # check if number of distinct characters in window is more
+#             # than K
+#             while len(table) > K:
+#                 startCharacter = String[start]
+#                 start += 1
+#                 table[startCharacter] -= 1
+#                 # if frequency becomes 0 then remove the character
+#                 if table[startCharacter] == 0:
+#                     table.pop(startCharacter)
+#
+#             # check if current window is greatest seen so far
+#             if (end - start + 1 > longest):
+#                 longest = end - start + 1
+#
+#         return longest
