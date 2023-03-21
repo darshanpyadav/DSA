202adee86d151f20de9c04f05910934b6b261c56
darshanpyadav93 <darshan.p@nutanix.com>
2023-03-21 13:19:25 +0530

    Prefix suffix problems

--- prefix_suffix_problems/maximum-sum-triplet.py
+++ prefix_suffix_problems/maximum-sum-triplet.py
@@ -44,15 +44,20 @@ Output 2:
  6
 '''
 
+import math
 from bisect import bisect_left
 
 
+# This function is used to get the left  most "index" where the
+# element is inserted in the sorted list
 def search_small(l, ele):
+    # bisect left is used to maintain order in sorted list
+    # gives index where ele should be inserted to maintain sorted order
     index = bisect_left(l, ele)
-    if index:
-        return index-1
-    else:
-        return -1
+    # We'll also update the sorted list by adding ele to current sorted list
+    l.insert(index, ele)
+    # index-1 will give the element smaller than the current "ele"
+    return index-1
 
 
 def solve(A):
@@ -66,13 +71,16 @@ def solve(A):
         max_sum_suffix_arr[i] = max(A[i], max_sum_suffix_arr[i+1])
 
     # Now calculate the element that is smaller than the current number frm the list that has elements from prev indices
-
-    max_sum = float('-inf')
+    max_sum = -math.inf
     sorted_prev_list = [A[0]]
-    for i in range(1, n-1):
-        res = search_small(sorted_prev_list, A[i])
-        if res != -1 and A[i] < max_sum_suffix_arr[i+1]:
-            max_sum = max(max_sum, sorted_prev_list[res] + A[i] + max_sum_suffix_arr[i+1])
-        sorted_prev_list.insert(res+1, A[i])
+    for cur_index in range(1, n-1):
+        smallest_index = search_small(sorted_prev_list, A[cur_index])
+        # smallest_index == -1 when there is no smallest element than A[cur_index] in sorted list
+        if smallest_index != -1 and A[cur_index] < max_sum_suffix_arr[cur_index+1]:
+            max_sum = max(max_sum, sorted_prev_list[smallest_index] + A[cur_index] + max_sum_suffix_arr[cur_index+1])
 
-    return max_sum
+    return max_sum if max_sum > 0 else 0
+
+
+for arr in [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]]:
+    print(solve(arr))
